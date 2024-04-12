af13114beda0eddf9ebe88b610ff0f8b3481e64d
codetechie5 <saurabhtripathi122222@gmail.com>
2024-04-12 14:43:20 +0530

    aall setup configured

--- src/app.js
+++ src/app.js
@@ -14,5 +14,12 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))//url se bhi data aayeg
 app.use(express.static("public"))
 app.use(cookieParser())
  
+//routes import
+import userRouter from './routes/user.routes.js'
+//routess deceleration
+app.use("/api/v1/users", userRouter)
+
+
+
 
 export {app}
---
+++ src/routes/user.routes.js
@@ -0,0 +1,24 @@
+// import {Router} from "express"
+// import { registerUser } from "../controllers/user.controllers"
+
+// const router = Router()
+
+// router.route("/register").post(registerUser)
+
+// export default router
+
+
+import { Router } from "express";
+import { 
+    registerUser
+} from "../controllers/user.controllers.js";
+
+
+const router = Router()
+
+router.route("/register").post(
+    registerUser
+    )
+
+
+export default router
